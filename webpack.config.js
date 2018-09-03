b84b6c90487735f84b9fcc7ec8053d306ff59217
Tristan Neumann <tristan.neumann@hotmail.com>
2018-09-03 10:51:53 -0700

    Set a background particle style;

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+var SRC_DIR = path.join(__dirname, './client/login/src');
+var DIST_DIR = path.join(__dirname, './client/login/dist');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
