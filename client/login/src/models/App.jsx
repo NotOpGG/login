04616f708f7ff423d23060da541dbc07a8468a5a
Tristan Neumann <tristan.neumann@hotmail.com>
2018-09-26 11:00:42 -0700

    correctly position input

--- client/login/src/models/App.jsx
+++ client/login/src/models/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from '../styles/login.css';
-console.log('styles: ', styles);
 class App extends React.Component {
   render() {
     return (
       <div className={styles.inputContainer}>
-      Hi from app
-        <input className={'form-control ' + styles.input} type="text" placeholder="Enter username" aria-label="Search"></input>
+        <input className={'form-control ' + styles.input} type="text" placeholder="Enter username"></input>
+        <button type="submit" className={'btn btn-primary ' + styles.submit}>Not OP</button>
       </div>
     );
   }
